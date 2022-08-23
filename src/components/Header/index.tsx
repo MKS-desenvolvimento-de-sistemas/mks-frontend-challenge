de55a2f843bc92895eb1f77fb9a2f5c945a7f2ea
vitor <tutornews2@gmail.com>
2022-08-23 13:13:38 -0300

    feat: add header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import {Container, HeaderHome} from './style';
+import {Container, HeaderHome, CartButton} from './style';
+import Logo from '../../img/logo.svg';
+import Cart from '../../img/cart.png';
 
 type Props = Record<string, unknown>;
 
 const Header = (props: Props) => (
 	<HeaderHome>
 		<Container>
-			<div>LOGO</div>
 			<div>
-				<i></i>
-				<span></span>
+				<img src={Logo} alt='Logo MKS sistemas' />
 			</div>
+			<CartButton>
+				<img src={Cart} alt='Carrinho de compra' />
+				<span>0</span>
+			</CartButton>
 		</Container>
 	</HeaderHome>
 );
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -5,8 +5,31 @@ export const Container = styled.div`
   max-width: 1216px;
   margin: 0 auto;
   height: 101px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const HeaderHome = styled.header`
   background: #0F52BA;
+`;
+
+export const CartButton = styled.header`
+  align-items: center;
+  background: white;
+  border-radius: 8px;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  height: 45px;
+  width: 90px;
+  
+  img {
+    margin-right: 16px;
+  }
+
+  span {
+    font-size: 18px;
+    font-weight: 700;
+  }
 `;
