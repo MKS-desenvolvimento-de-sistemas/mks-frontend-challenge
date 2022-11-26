f10b95afd8107df6f103396695daac7979dd5880
andm3ii0 <andersonmello722@gmail.com>
2022-11-26 10:43:50 -0300

    organizando arquivos

--- src/components/ProductCardCart.tsx
+++ src/components/ProductCardCart.tsx
@@ -22,52 +22,6 @@ interface Props extends Product {
   removeItemFromCart: Function;
 }
 
-// function ProductCardCart({
-//   img,
-//   title,
-//   price,
-//   count,
-//   id,
-//   ...props
-// }: Product) {
-//   const oneLess = () => {
-//     const { removeProduct, updateValue } = props as ReduxProps;
-//     removeProduct(id);
-//     updateValue(-(Number(price.split(".")[0])));
-//   };
-
-//   const oneMore = () => {
-//     const { addProduct, updateValue } = props as ReduxProps;
-//     addProduct(id);
-//     updateValue(Number(price.split(".")[0]));
-//   };
-
-//   const remove = () => {
-//     const { removeItemFromCart, updateValue } = props as ReduxProps;
-//     updateValue(-(Number(price.split(".")[0]) * count));
-//     removeItemFromCart(id)
-//   }
-
-//   return (
-//     <div className="product-card-cart">
-//       <img src={img} alt={title} />
-//       <p className="title">{title}</p>
-//       <div className="counter">
-//         qtd
-//         <div>
-//           <button onClick={oneLess}>-</button>
-//           <p>{count}</p>
-//           <button onClick={oneMore}>+</button>
-//         </div>
-//       </div>
-//       <p className="price">{`R$${
-//         (price.split(".")[0] as unknown as number) * count
-//         }`}</p>
-//       <button onClick={remove} className="remove-item">X</button>
-//     </div>
-//   );
-// }
-
 class ProductCardCart extends React.Component<Props> {
   oneLess = () => {
     const { id, price } = this.props;
