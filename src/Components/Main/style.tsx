7d41abd79332bfc04ef2cecd4dc9881a68407a02
Christian <christian.mf22@gmail.com>
2023-01-29 22:46:51 -0300

    fix: Adjusted responsive size of the main page

--- src/Components/Main/style.tsx
+++ src/Components/Main/style.tsx
@@ -4,12 +4,20 @@ export const MainStyle = styled.main``;
 
 export const ProductsList = styled.ul`
   max-width: 990px;
-	margin: 0 auto;
-	
-	display: flex;
+  margin: 0 auto;
+
+  display: flex;
   flex-direction: row;
   flex-wrap: wrap;
   align-items: center;
   justify-content: center;
   align-content: center;
+
+  @media (max-width: 950px) {
+    margin-top: 88px;
+  }
+
+  @media (max-width: 720px) {
+    margin-top: 48px;
+  }
 `;
