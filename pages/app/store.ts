7d7b78c3709a3dbde04acbcd636c7706c162ee6e
Júnior Nogueira <haroldognjunior@ggmail.com>
2023-01-31 17:19:13 -0400

    initial commit

--- pages/app/store.ts
+++ pages/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from './counter/counterSlice';
 import productsReducer from './state/actions/productsReducers';
 
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
+  reducer: {   
     products: productsReducer
   },
 });
