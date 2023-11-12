63139888d90073d0fce6f05f941151c072326180
Pietro Bucker <pietro_177@hotmail.com>
2023-11-12 20:58:14 -0300

    importacao styles, Header, Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import { useQuery } from '@tanstack/react-query';
 import { FC } from 'react';
+import { styled } from 'styled-components';
 import { RequestAPI, ProductList } from './models/requestAPI';
+import Header from './components/Header';
+import './App.css';
+import Card from './components/Card';
+
+const StyledHome = styled.div`
+  width: 100%;
+`;
 
 const App: FC = () => {
   const { data, isError, isLoading } = useQuery<ProductList>({
@@ -10,8 +18,10 @@ const App: FC = () => {
   console.log(data);
 
   return (
+    <StyledHome>
 
-    <div>
+      <Header />
+      <Card />
       {isLoading && <p>Loading...</p>}
       {isError && <p>Algo deu errado tente novamente</p>}
       {data && data.products.map((product) => (
@@ -21,7 +31,7 @@ const App: FC = () => {
           <p>{product.price}</p>
         </div>
       ))}
-    </div>
+    </StyledHome>
 
   );
 };
