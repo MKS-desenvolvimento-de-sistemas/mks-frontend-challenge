b25cc99d4376e5e878dde76563ba6c3b9be41faf
Pietro Bucker <pietro_177@hotmail.com>
2023-11-14 00:42:09 -0300

    botao para abrir e fechar cart

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,29 @@
-import { FC } from 'react';
+import { FC, useContext, useState } from 'react';
 import { StyledButton, StyledCard, StyledLogo, StyledText } from '../styles/headerStyles';
+import ShoppingCart from './ShoppingCart';
+import CartContext from '../context/cartContext';
 
 const Header: FC = () => {
+  const [showCart, setShowCart] = useState(false);
+  const handleClick = () => {
+    if (showCart) {
+      setShowCart(false);
+    }
+    setShowCart(true);
+  };
   return (
     <StyledCard>
       <StyledLogo>
         <h1>MKS</h1>
         <StyledText>Sistemas</StyledText>
       </StyledLogo>
-      <StyledButton type="button">
+      <StyledButton type="button" onClick={ handleClick }>
         🛒
         {' '}
         <strong style={ { marginLeft: '0.5rem' } }>0</strong>
       </StyledButton>
+      {showCart && <ShoppingCart />}
+
     </StyledCard>
   );
 };
