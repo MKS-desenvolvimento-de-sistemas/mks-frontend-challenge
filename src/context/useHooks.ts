eccfe4c01ba5cb2e155ca9c080292798644b6243
Pietro Bucker <pietro_177@hotmail.com>
2023-11-14 17:07:07 -0300

    criado novo estado showCart

--- src/context/useHooks.ts
+++ src/context/useHooks.ts
@@ -4,6 +4,7 @@ import { ProductList } from '../interface/type';
 export const useHookCarts = () => {
   // array com todas as receitas
   const [cart, setCart] = useState<ProductList>({ products: [] });
+  const [showCart, setShowCart] = useState<boolean>(false);
 
-  return { cart, setCart };
+  return { cart, setCart, showCart, setShowCart };
 };
