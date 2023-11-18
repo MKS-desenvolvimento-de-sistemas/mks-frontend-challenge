10a77d554c0430b8b8a6f737aa2a312de369e57a
VictorMFL <vm859222@gmail.com>
2023-11-18 16:07:06 -0300

    feat: inciando projeto em next-js

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+import './globals.css'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Front-End Challenge',
+  description: 'My resolution of front end challenge',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pt-br">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
