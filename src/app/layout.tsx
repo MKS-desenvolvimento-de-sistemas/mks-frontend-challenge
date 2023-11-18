e4992be7b53034c61f749fab4b7d83d4005fc0a9
VictorMFL <vm859222@gmail.com>
2023-11-18 16:47:10 -0300

    chore: configurando o styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,25 @@
-import type { Metadata } from 'next'
-import { Montserrat } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "../styles/globals.css";
+import Providers from "@/providers/Providers";
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Front-End Challenge',
-  description: 'My resolution of front end challenge',
-}
+  title: "Front-End Challenge",
+  description: "My resolution of front end challenge",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="pt-br">
-      <body className={montserrat.className}>{children}</body>
-    </html>
-  )
+    <Providers>
+      <html lang="pt-br">
+        <body className={montserrat.className}>{children}</body>
+      </html>
+    </Providers>
+  );
 }
