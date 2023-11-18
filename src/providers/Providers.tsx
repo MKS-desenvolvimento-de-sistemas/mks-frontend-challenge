af800abd6b811e7e831e58e7aec8b31044754492
VictorMFL <vm859222@gmail.com>
2023-11-18 16:55:03 -0300

    chore: add axios e react-query configurado

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
 
 import { ProviderStyle } from "@/styles/Theme";
 import { ReactNode } from "react";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
 const Providers = ({ children }: { children: ReactNode }) => {
-  return <ProviderStyle>{children}</ProviderStyle>;
+  return (
+    <ProviderStyle>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ProviderStyle>
+  );
 };
 
 export default Providers;
