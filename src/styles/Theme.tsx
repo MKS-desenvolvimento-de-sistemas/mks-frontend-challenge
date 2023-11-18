8034e9a3ece6d7a8553a0a718c709ab180d3b138
VictorMFL <vm859222@gmail.com>
2023-11-18 19:08:15 -0300

    chore: add cores no Theme.tsx

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -5,6 +5,13 @@ const theme = {
   colors: {
     white: "#fff",
     black: "#000",
+    blue01: "#0F52BA",
+    background: "#F9F9F9",
+    bgFooter: "#EEE",
+    card: {
+      text: "#2C2C2C",
+      bgPrice: "#373737",
+    },
   },
 };
 
