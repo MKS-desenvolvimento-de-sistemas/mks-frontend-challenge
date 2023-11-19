33685d86677a9629a7bacbf3e639c4b3851d2e30
dreisdev <dreis.dev@outlook.com>
2023-11-19 15:45:27 -0300

    DeployConfigNetfly

--- mks-app-challenge/.next/static/zM5vx0Q50EyHPLrjFMzNt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/38-7e3fe78e0e54be21.js","static/chunks/pages/index-c3965fac18fd1425.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
