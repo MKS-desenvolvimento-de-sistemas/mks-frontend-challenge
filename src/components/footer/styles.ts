eb3385b1edb1d6dea77df789dd0b8aeecf35f076
VictorMFL <vm859222@gmail.com>
2023-11-20 21:27:26 -0300

    test: testando componente <Footer />

--- src/components/footer/styles.ts
+++ src/components/footer/styles.ts
@@ -3,14 +3,14 @@ import styled from "styled-components";
 export const Container = styled.footer`
   width: 100%;
   height: 34px;
-  background-color: ${(props) => props.theme.colors.bgFooter};
+  background-color: ${(props) => props.theme.colors?.bgFooter};
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 export const FooterText = styled.p`
-  color: ${(props) => props.theme.colors.black};
+  color: ${(props) => props.theme.colors?.black};
   font-size: 12px;
   font-weight: 400;
 `;
