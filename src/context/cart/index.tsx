52a7b7263fb9c056d22566d9cab49a8bc909ab5a
Luis Eduardo Hoshina <duhoshina@gmail.com>
2024-04-17 05:15:44 -0300

    feat: contexto do carrinho

--- src/context/cart/index.tsx
+++ src/context/cart/index.tsx
@@ -1,6 +1,11 @@
-import { Dispatch, SetStateAction } from "react";
+import ProductProps from "@/components/product/type";
+import { createContext, Dispatch, SetStateAction, useContext } from "react";
 
 interface ICartContext {
   isCartActive: boolean;
   setCartActive: Dispatch<SetStateAction<boolean>>;
+  products: ProductProps[];
+  setProducts: Dispatch<SetStateAction<ProductProps[]>>;
 };
+
+export const CartContext = createContext<ICartContext | undefined>(undefined);
